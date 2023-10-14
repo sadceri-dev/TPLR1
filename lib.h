2d3c688555a06adeb0f01acf22d422a93cac87fe
sadceri <70819311+sadceri-dev@users.noreply.github.com>
2023-10-14 19:08:38 +0300

    Update files
    Обновление файлов

---
+++ lib.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include "Bibliary.h"
+#include "Books.h"
+#include "Textbook.h"
+#include "Canselar.h"
+#include "Keeper.h"
+#include "Menu.h"
+
+#endif
